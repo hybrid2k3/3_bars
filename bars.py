6d23d05bbc879a748ef457c0e547cc694853fd12
hybrid2k3 <hybr@mail.ru>
2017-12-20 16:50:10 +0300

    reworked

--- bars.py
+++ bars.py
@@ -20,34 +20,34 @@ def load_data(filepath):
     with open(filepath, encoding='utf-8') as json_file:
         json_data = json.load(json_file)
         lenjson = (len(json_data['features']))  # считаем кол-во элементов
-        i = 0
+        count = 0
         all_seatcount = []
         all_address = []
         all_coordins = []
         all_name = []
         all_bars_list = []
-        while i < lenjson:  # создаем списки
-            bar_seats_count = json_data['features'][i]['properties']['Attributes']['SeatsCount']
-            bar_address = json_data['features'][i]['properties']['Attributes']['Address']
-            bar_name = json_data['features'][i]['properties']['Attributes']['Name']
-            lat_long = json_data['features'][i]['geometry']['coordinates']
+        while count < lenjson:  # создаем списки
+            bar_seats_count = json_data['features'][count]['properties']['Attributes']['SeatsCount']
+            bar_address = json_data['features'][count]['properties']['Attributes']['Address']
+            bar_name = json_data['features'][count]['properties']['Attributes']['Name']
+            lat_long = json_data['features'][count]['geometry']['coordinates']
             all_seatcount.append(bar_seats_count)
             all_address.append(bar_address)
             all_coordins.append(lat_long)
             all_name.append(bar_name)
             all_bars_list.append([bar_name, bar_address, lat_long, bar_seats_count])
-            i += 1
+            count += 1
         return all_bars_list
 
 
 def get_biggest_bar(json_data):
     big_bar_count = []
-    i = 0
-    while i < len(json_data):
+    count = 0
+    while count < len(json_data):
         for element in json_data:
             big_bar = element[3]
             big_bar_count.append(big_bar)
-            i = i + 1
+            count +=1
     max_seat_count = (max(big_bar_count))
     max_seat_index = big_bar_count.index(max_seat_count)
     print('Самый большой бар называется - {}. Находится по адресу {}, кол-во посадочных мест равно - {}'.format(
@@ -56,12 +56,12 @@ def get_biggest_bar(json_data):
 
 def get_smallest_bar(json_data):
     small_bar_count = []
-    i = 0
-    while i < len(json_data):
+    count = 0
+    while count < len(json_data):
         for element in json_data:
             small_bar = element[3]
             small_bar_count.append(small_bar)
-            i = i + 1
+            count +=1
     max_seat_count = (min(small_bar_count))
     max_seat_index = small_bar_count.index(max_seat_count)
     print('Самый маленький бар называется - {}. Находится по адресу {}, кол-во посадочных мест равно - {}'.format(
@@ -70,12 +70,12 @@ def get_smallest_bar(json_data):
 
 def get_closest_bar(json_data, longitude, latitude):
     distance = []
-    i = 0
-    while i < len(json_data):
+    count = 0
+    while count < len(json_data):
         for element in json_data:
             dist = haversine(latitude, longitude, element[2][1], element[2][0])
             distance.append(dist)
-            i = i + 1
+            count +=1
     round_distance = (round(min(distance), 2))
     minimal_dst_bar = min(distance)
     bar_index = distance.index(minimal_dst_bar)
